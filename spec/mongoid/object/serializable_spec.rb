1b6f41cc8de4c9dc7992f9b2358c20e7a8a2ad71
Minwoo Lee <ermaker@gmail.com>
2016-06-10 15:34:02 +0900

    Add DummyObject

--- spec/mongoid/object/serializable_spec.rb
+++ spec/mongoid/object/serializable_spec.rb
@@ -1,2 +1,12 @@
+require 'mongoid'
+
+class DummyObject
+  include Mongoid::Document
+  include Mongoid::Object::Serializable
+end
 RSpec.describe Mongoid::Object::Serializable do
+  specify { expect(DummyObject).to include(described_class) }
+  describe DummyObject do
+    it { is_expected.to include(Mongoid::Document) }
+  end
 end
