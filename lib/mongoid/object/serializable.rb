69d6e3e20cc8365355598f73a7f164a55a405ae3
Minwoo Lee <ermaker@gmail.com>
2016-06-10 14:45:13 +0000

    Add .consume

--- lib/mongoid/object/serializable.rb
+++ lib/mongoid/object/serializable.rb
@@ -5,12 +5,24 @@ module Mongoid
         base.const_set('Document', Class.new)
         base::Document.include Mongoid::Document
         base::Document.field :object, type: Dynamic
+
+        base.extend(ClassMethods)
       end
 
       def save
         document = self.class::Document.new
         document.object = self
         document.save
+      end
+
+      module ClassMethods
+        def consume
+          return enum_for(:consume) unless block_given?
+          self::Document.each do |document|
+            yield document.object
+            document.delete
+          end
+        end
       end
     end
   end
--- spec/mongoid/object/serializable_spec.rb
+++ spec/mongoid/object/serializable_spec.rb
@@ -19,13 +19,47 @@ RSpec.describe Mongoid::Object::Serializable do
       specify { expect(described_class).to include(Mongoid::Document) }
     end
 
-    specify do
-      subject.x = 1
-      subject.y = 2
-      subject.save
-      actual = described_class::Document.first.object
-      expect(actual.x).to eq(1)
-      expect(actual.y).to eq(2)
+    describe 'with variables' do
+      before do
+        subject.a = 1
+        subject.b = :b
+        subject.c = 'c'
+        subject.d = [1, :b, 'c', [{}]]
+        subject.e = { a: { b: [{}] } }
+        subject.save
+      end
+      let(:actual) { described_class::Document.first.object }
+
+      specify { expect(actual.a).to eq(1) }
+      specify { expect(actual.b).to eq(:b) }
+      specify { expect(actual.c).to eq('c') }
+      specify { expect(actual.d).to eq([1, :b, 'c', [{}]]) }
+      specify { expect(actual.e).to eq(a: { b: [{}] }) }
+    end
+
+    describe 'with objects' do
+      let(:size) { 10 }
+      before do
+        size.times do |idx|
+          described_class.new.tap { |obj| obj.value = idx }.save
+        end
+      end
+
+      specify do
+        expect do
+          described_class.consume {}
+        end.to change { described_class::Document.all.size }.by(-size)
+      end
+
+      specify do
+        expect(described_class.consume.map(&:value)).to eq([*0..9])
+      end
+
+      specify do
+        expect do
+          described_class.consume(&:save)
+        end.not_to change { described_class::Document.all.size }
+      end
     end
   end
 end
